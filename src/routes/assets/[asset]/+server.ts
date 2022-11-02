20b7b0b159016d36443daa9e596c60e480cc3e88
revers3ntropy <josephcoppin@gmail.com>
2022-11-02 14:27:50 +0000

    assets placeholder api

---
+++ src/routes/assets/[asset]/+server.ts
@@ -0,0 +1,18 @@
+import type { RequestHandler } from "@sveltejs/kit";
+import { encode } from "fast-png";
+
+export const GET: RequestHandler = async ({ cookies, params }) => {
+    // placeholder image
+    const image = encode({
+        width: 500,
+        height: 500,
+        data: new Uint8Array(500 * 500 * 4).fill(255),
+    });
+
+    return new Response(image, {
+        headers: {
+            'Content-Type': 'image/png',
+            'Cache-Control': 'max-age=31536000, immutable'
+        }
+    });
+}
