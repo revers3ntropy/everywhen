55408332173874718de173554bb94d9ffc6dc091
revers3ntropy <josephcoppin@gmail.com>
2023-02-15 19:14:14 +0000

    Run linter
    Signed-off-by: revers3ntropy <josephcoppin@gmail.com>

--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -12,38 +12,37 @@ export const POST: RequestHandler = async ({ request, cookies }) => {
 	const { username, password } = await request.json();
 
 	if (!username || username.length < 3 || typeof username !== "string") {
-		return new Response(
-			JSON.stringify({ error: "Username must be at least 3 characters" }),
-			{ status: 400 }
-		);
-	}
-	if (!password || password.length < 8 || typeof password !== "string") {
-		return new Response(
-			JSON.stringify({ error: "Password must be at least 8 characters" }),
-			{ status: 400 }
-		);
-	}
+        return new Response(
+            JSON.stringify({ error: "Username must be at least 3 characters" }),
+            { status: 400 }
+        );
+    }
+    if (!password || password.length < 8 || typeof password !== "string") {
+        return new Response(
+            JSON.stringify({ error: "Password must be at least 8 characters" }),
+            { status: 400 }
+        );
+    }
 
-	const existingUsers = await query`
-		SELECT *
-		FROM users
-		WHERE username = ${ username }
-	`;
+    const existingUsers = await query`
+        SELECT *
+        FROM users
+        WHERE username = ${ username }
+    `;
 	if (existingUsers.length !== 0) {
-		return new Response(
-			JSON.stringify({ error: "Username already exists" }),
-			{ status: 400 }
-		);
-	}
+        return new Response(JSON.stringify({ error: "Username already exists" }), {
+            status: 400
+        });
+    }
 
-	let salt = "";
+    let salt = "";
 	while (true) {
 		salt = randomString(10);
-		const existingSalts = await query`
-			SELECT salt
-			FROM users
-			WHERE salt = ${ salt }
-		`;
+        const existingSalts = await query`
+            SELECT salt
+            FROM users
+            WHERE salt = ${ salt }
+        `;
 		if (existingSalts.length === 0) {
 			break;
 		}
@@ -52,14 +51,14 @@ export const POST: RequestHandler = async ({ request, cookies }) => {
 	const id = await generateUUId();
 	const now = Math.round(Date.now() / 1000);
 
-	await query`
-		INSERT INTO users (id, username, password, salt, created)
-		VALUES (${ id },
-				${ username },
-				SHA2(${ password + salt }, 256),
-				${ salt },
-				${ now });
-	`;
+    await query`
+        INSERT INTO users (id, username, password, salt, created)
+        VALUES (${ id },
+                ${ username },
+                SHA2(${ password + salt }, 256),
+                ${ salt },
+                ${ now });
+    `;
 
 	cookies.set(KEY_COOKIE_KEY, password, AUTH_COOKIE_OPTIONS);
 	cookies.set(USERNAME_COOKIE_KEY, username, AUTH_COOKIE_OPTIONS);
