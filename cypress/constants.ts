0a7d720774aa8ff95655833e46753fb0dc0f5e3b
revers3ntropy <josephcoppin@gmail.com>
2023-02-23 12:52:39 +0000

    Added Cypress integration
    Signed-off-by: revers3ntropy <josephcoppin@gmail.com>

---
+++ cypress/constants.ts
@@ -0,0 +1,2 @@
+export const LOCALHOST_PORT = 5173;
+export const ROOT = `http://localhost:${LOCALHOST_PORT}`;
---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,7 @@
+import { ROOT } from "../constants";
+
+describe('Create New Account', () => {
+  it('passes', () => {
+    cy.visit(ROOT)
+  })
+})
