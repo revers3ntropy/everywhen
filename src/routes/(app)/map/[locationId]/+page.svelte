82457962e9ed5c65732bb94bb8dec36be3f34f45
revers3ntropy <josephcoppin@gmail.com>
2023-09-15 11:18:19 +0100

    fix layout of location page
    Signed-off-by: revers3ntropy <josephcoppin@gmail.com>

--- src/routes/(app)/map/[locationId]/+page.svelte
+++ src/routes/(app)/map/[locationId]/+page.svelte
@@ -3,6 +3,7 @@
     import EditLocation from '$lib/components/location/EditLocation.svelte';
     import Entries from '$lib/components/entry/Entries.svelte';
     import type { Location } from '$lib/controllers/location/location';
+    import { navExpanded } from '$lib/stores';
     import type { PageData } from './$types';
 
     export let data: PageData;
@@ -18,20 +19,18 @@
 <svelte:head>
     <title>{location.name} | Location</title>
 </svelte:head>
+<main class="p-4 {$navExpanded ? 'md:ml-48' : 'md:ml-16'}">
+    <section class="p-2 md:rounded-lg md:bg-vLightAccent md:p-4">
+        <EditLocation {...location} {onChange} />
+    </section>
 
-<section class="container">
-    <EditLocation {...location} {onChange} />
-</section>
-
-<section>
-    <Entries
-        options={{
-            locationId: location.id
-        }}
-        showLabels
-        {locations}
-    />
-</section>
-
-<style lang="scss">
-</style>
+    <section class="pt-4">
+        <Entries
+            options={{
+                locationId: location.id
+            }}
+            showLabels
+            {locations}
+        />
+    </section>
+</main>
