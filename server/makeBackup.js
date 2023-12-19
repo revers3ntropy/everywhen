acd66931ce3168dbb2ae7971a1054e93d9c68a7b
revers3ntropy <josephcoppin@gmail.com>
2023-12-19 16:05:12 +0000

    use correct value in makeBackup script to be replaced with current deployment target
    Signed-off-by: revers3ntropy <josephcoppin@gmail.com>

--- server/makeBackup.js
+++ server/makeBackup.js
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const env = '$ENV$';
+const env = '%ENV%';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
