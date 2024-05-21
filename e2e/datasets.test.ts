a140d3233e66a1f91f2596d2a0b4330ef37762ca
revers3ntropy <josephcoppin@gmail.com>
2024-05-21 12:15:09 +0100

    remove 'record weight' e2e test
    Signed-off-by: revers3ntropy <josephcoppin@gmail.com>

--- e2e/datasets.test.ts
+++ e2e/datasets.test.ts
@@ -1,30 +1,8 @@
 import { expect, test } from '@playwright/test';
-import { expectDeleteUser, generateUserAndSignIn } from './lib/helpers';
 
 test.describe('Datasets', () => {
     test('Cannot visit page without authentication', async ({ page }) => {
         await page.goto('/datasets', { waitUntil: 'networkidle' });
         await expect(page).toHaveURL('/login?redirect=datasets');
-    });
-
-    test('Can record weight', async ({ page }) => {
-        const { api, auth } = await generateUserAndSignIn(page);
-        await page.goto('/datasets', { waitUntil: 'networkidle' });
-        await expect(page).toHaveURL('/datasets');
-
-        await page.getByRole('button', { name: "Start Recording 'Weight'" }).click();
-        await page.goto('/journal', { waitUntil: 'networkidle' });
-
-        // as dataset changes are not propagated client-side yet
-        await page.reload();
-
-        await page.getByLabel('enter your weight').fill('10');
-        await page.getByLabel('Submit Weight').click();
-        await expect(page.getByText('Weight entered')).toBeAttached();
-
-        await page.getByLabel('Submit Weight').click();
-        await expect(page.getByText('Invalid weight')).toBeAttached();
-
-        await expectDeleteUser(api, auth);
     });
 });
