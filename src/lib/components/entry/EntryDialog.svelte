42b1130730e6cddae39d2b30b1e715f63963f032
revers3ntropy <josephcoppin@gmail.com>
2025-03-12 09:44:53 +0000

    fix y overflow of long entries in dialogs
    Signed-off-by: revers3ntropy <josephcoppin@gmail.com>

--- src/lib/components/entry/EntryDialog.svelte
+++ src/lib/components/entry/EntryDialog.svelte
@@ -24,7 +24,7 @@
     });
 </script>
 
-<div class="min-h-[calc(100vh - 100px)] max-w-full overflow-x-hidden">
+<div class="min-h-[calc(100vh - 100px)] max-h-[90vh] overflow-y-auto overflow-x-hidden">
     {#if entry && entry.ok}
         {#key locations}
             <Entry
