07fc2fe315aff3b122245f4c66037db364557b64
revers3ntropy <josephcoppin@gmail.com>
2025-08-09 16:11:35 +0100

    remove unwrapping of non-result

--- src/routes/(app)/journal/deleted/+page.server.ts
+++ src/routes/(app)/journal/deleted/+page.server.ts
@@ -1,12 +1,11 @@
 import { Label } from '$lib/controllers/label/label.server';
-import { error } from '@sveltejs/kit';
 import { Location } from '$lib/controllers/location/location.server';
 import { cachedPageRoute } from '$lib/utils/cache.server';
 import type { PageServerLoad } from './$types';
 
 export const load = cachedPageRoute(async auth => {
     return {
-        locations: (await Location.all(auth)).unwrap(e => error(500, e)),
+        locations: await Location.all(auth),
         labels: await Label.allIndexedById(auth)
     };
 }) satisfies PageServerLoad;
