0ab90ab7687d349e4a8d65560b01e2848b79848b
revers3ntropy <josephcoppin@gmail.com>
2025-08-10 13:39:24 +0100

    new logos

--- src/lib/components/ui/Logo.svelte
+++ src/lib/components/ui/Logo.svelte
@@ -6,14 +6,6 @@
     const HEIGHT = 512;
 
     export let scale = 1 / 4;
-
-    // use much more compressed version of logo when scale is small
-    $: useMin = scale < 0.9;
 </script>
 
-<img
-    src="/logo{$theme}{useMin ? '-min' : ''}.png"
-    alt="Logo"
-    height={HEIGHT * scale}
-    width={WIDTH * scale}
-/>
+<img src="/logo-{$theme}.png" alt="Logo" height={HEIGHT * scale} width={WIDTH * scale} />
