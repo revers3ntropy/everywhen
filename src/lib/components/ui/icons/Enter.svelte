19283c5bf0379b12500ec4c00b87db4115128f23
revers3ntropy <josephcoppin@gmail.com>
2025-08-15 19:50:57 +0100

    add MacCommand and Enter icons

---
+++ src/lib/components/ui/icons/Enter.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    export let size = '1rem';
+    export let color = 'currentColor';
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="10 12 20 16" width={size} height={size}>
+    <g>
+        <g>
+            <g>
+                <g>
+                    <path
+                        fill={color}
+                        d="M14,22h10c0.8,0,1.5-0.7,1.5-1.5v-6c0-0.3,0.2-0.5,0.5-0.5s0.5,0.2,0.5,0.5v6c0,1.4-1.1,2.5-2.5,2.5H14
+					V22z"
+                    />
+                </g>
+                <g>
+                    <path
+                        fill={color}
+                        d="M17,26c-0.1,0-0.3,0-0.4-0.1l-3-3c-0.2-0.2-0.2-0.5,0-0.7l3-3c0.2-0.2,0.5-0.2,0.7,0s0.2,0.5,0,0.7
+					l-2.6,2.6l2.6,2.6c0.2,0.2,0.2,0.5,0,0.7C17.3,26,17.1,26,17,26z"
+                    />
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
